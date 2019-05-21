544128c20e2a662816636e3083e5067de5cae80f
Daniel Cook <danielecook@gmail.com>
2019-05-21 15:49:37 +0100

    Update name to avoid confusion

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -6,7 +6,7 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 set -o nounset
 
 # slice
-run vcf2json sk
+run vcf2json sc
 assert_exit_code 0
 assert_no_stderr
 assert_equal "18" $(cat $STDOUT_FILE | wc -l)
